eb69aa3477c82b55a6cd36d61a74390f54cab8a3
shengjie_yan <ysjshengjie@live.cn>
2017-09-06 05:34:26 +0800

    Empty template

--- Learning.NetCore.Empty/Program.cs
+++ Learning.NetCore.Empty/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Learning.NetCore.EmptyWeb
+namespace Learning.NetCore.Empty
 {
     public class Program
     {
--- Learning.NetCore.Empty/Startup.cs
+++ Learning.NetCore.Empty/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Learning.NetCore.EmptyWeb
+namespace Learning.NetCore.Empty
 {
     public class Startup
     {
